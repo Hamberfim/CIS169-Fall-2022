c25f367c1de58c92f5637768f5bea3785a0d2efd
Hamberfim <adhamlingithub@gmail.com>
2022-08-23 12:29:29 -0500

    module 0 nearly completed

---
+++ Module0/Topic4/HelloWorldHamlin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/***************************************************************
+* Name         : HelloWorldHamlin
+* Author       : Anthony Hamlin
+* Created      : 08/23/2022
+* Course       : CIS 169 - C#
+* Version      : 1.0
+* OS           : Windows 10
+* IDE          : Visual Studio 2019 Community
+* Copyright    : This is my own original work based on
+*                      specifications issued by our instructor
+* Description  : This program is the quintessential Hello World
+*                      Input :  None
+*                      Output: String Hello World!
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or
+* unmodified. I have not given other fellow student(s) access
+* to my program.        
+***************************************************************/
+namespace HelloWorldHamlin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string variable 
+            string professor = "Dr. M. E. Ruse";
+           // ouput with a little flair 
+            Console.WriteLine($"Hello World! Hello {professor}!");
+        }
+    }
+}
