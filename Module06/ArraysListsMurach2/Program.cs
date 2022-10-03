0c504a4484f00fd7ab70f8f95dc0fe5edb51f055
Hamberfim <adhamlingithub@gmail.com>
2022-10-03 13:37:14 -0500

    ArrayList List<> practice Murach

---
+++ Module06/ArraysListsMurach2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ArraysListsMurach2
+{
+    class Program
+    {
+        // return an array
+        public static string[] ReturnRandAdj()
+        {
+            Random rnd = new Random();
+            int selectOne = rnd.Next(0, 7);
+            int selectTwo = rnd.Next(0, 7);
+            string[] returnArr = new string[2];
+            string[] encouraging = { "helpful", "kindly", "gracious", "encouraging", "cautious", "keen", "enthusiastic" };
+            returnArr[0] = encouraging[selectOne];
+            returnArr[1] = encouraging[selectTwo];
+
+            return returnArr;
+        }
+
+        static void Main(string[] args)
+        {
+            // return an array
+            string[] rndAdjs = ReturnRandAdj();
+            foreach (var item in rndAdjs)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+            // array element range use
+            string[] adjDesc = { "shocking", "melodic", "heavy", "hypnotic", "harsh", "burly", "somber" };
+            // range start to end
+            var startEnd = adjDesc[0..7];
+            foreach (var item in startEnd)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            var middle = adjDesc[2..5];
+            foreach (var item in middle)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Module06/ArraysListsMurach3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace ArraysListsMurach3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List Collections - Untyped vs Typed
+            // untyped ArrayList
+            ArrayList nums1 = new ArrayList();
+            nums1.Add(99);
+            nums1.Add(35);
+            nums1.Add(16);
+            nums1.Add("Pizza");  // Will compile -RUN TIME ERROR  ???!!!
+            foreach (var item in nums1)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+            // Typed List
+            List<int> nums2 = new List<int>();
+            nums2.Add(12);
+            nums2.Add(23);
+            nums2.Add(45);
+            // nums2.Add("Pizza");  // won't compile - prevent runtime error
+            foreach (var item in nums2)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+            // insert into a List insert() 
+            nums2.Insert(1, 15);
+            nums2.Insert(3, 30);  // need to know that 15 is now in index pos 1 and that 23 moved to index pos 2
+            foreach (var item in nums2)
+            {
+                Console.Write($"{item} ");  // Expected: 12, 15, 23, 30, 45
+            }
+            Console.WriteLine();
+
+            // remove from List removeAt(index) 
+            nums2.RemoveAt(2);
+            foreach (var item in nums2)
+            {
+                Console.Write($"{item} ");  // Expected: 12, 15, 30, 45
+            }
+            Console.WriteLine();
+
+            // declared
+            SortedList<string, double> myPairs = new SortedList<string, double>(2);
+            myPairs.Add("Pi", 3.14159265);
+            myPairs.Add("Golden Ratio", 1.6180);
+
+
+            // delcared/initialized
+            SortedList<string, double> otherPairs = new SortedList<string, double>
+            {
+                {"Euler's Num",  2.7182}, { "Speed of Light MPS", 186282.0 }
+            };
+
+            // declared/initialized with an index
+            SortedList<string, decimal> indexPairs = new SortedList<string, decimal>
+            {
+                ["pizza"] = 12.99m,
+                ["tacos"] = 4.99m,
+                ["pasta"] = 2.99m,
+                ["nachos"] = 1.99m,
+            };
+
+            foreach (var item in indexPairs)
+            {
+                Console.Write($"{item} ");  // order is sorted by key
+            }
+            Console.WriteLine();
+
+            // remove from list remove(key)
+            indexPairs.Remove("pasta");
+            foreach (var item in indexPairs)
+            {
+                Console.Write($"{item} ");  // order is sorted by key
+            }
+            Console.WriteLine();
+
+            // ContainsKey()/ContainsValue()  - return bool
+            Console.WriteLine(indexPairs.ContainsKey("tacos"));
+            Console.WriteLine(myPairs.ContainsValue(1.6180));
+
+            // KeyValuePair Loop
+            foreach (KeyValuePair<string, decimal> item in indexPairs)
+            {
+                Console.Write($"{item} ");
+            }
+
+        }
+    }
+}
