1e25b1e9bfec43f61690c07e219d31cdb273d948
Hamberfim <adhamlingithub@gmail.com>
2022-10-24 13:06:42 -0500

    Topic 1, 2 completed

---
+++ Module010/Topic02/Book/Book.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    /***************************************************************
+    * Name         : Book - interface
+    * Author       : Anthony Hamlin
+    * Created      : 10/24/2022
+    * Course       : CIS 169 - C#
+    * Version      : 1.0
+    * OS           : Windows 10
+    * IDE          : Visual Studio 2019 Community
+    * Copyright    : This is my own original work based on
+    *                      specifications issued by our instructor
+    * Description  : This program creates a book class
+    *                      Input :  Fields in the class - author, title, keywords, publication date and ISBN.
+    *                      Output: a book instance
+    * Academic Honesty: I attest that this is my original work.
+    * I have not used unauthorized source code, either modified or
+    * unmodified. I have not given other fellow student(s) access
+    * to my program.        
+    ***************************************************************/
+    class Book : ICloneable
+    {
+
+        // Create a class, called Book with properties for an author, title, keywords, publication date and ISBN.
+        // Submit Book.cs with Academic Honesty Header included.
+        // Extra Credit: Use a collection for keywords.The collections you have used are array and List.
+        // Which is more appropriate when not all books will have the same number of key words? (up to 3 points)
+
+        // drive in Program.cs
+
+        // fields/properties
+        private string _author;
+        private string _title;
+        private string _keywords;
+        private string _pubDate;
+        private long _ISBN;
+
+        // no-arg constructor
+        public Book()
+        {
+            Author = "";
+            Title = "";
+            Keywords = "";
+            PubDate = "";
+            ISBN = 0;
+        }
+
+        // constructor 
+        public Book(string author, string title, string keywords, string pubDate, long ISBN)
+        {
+            Author = author;
+            Title = title;
+            Keywords = keywords;
+            PubDate = pubDate;
+            this.ISBN = ISBN;
+        }
+
+        // set/get
+        public string Author { get => _author; set => _author = value; }
+        public string Title { get => _title; set => _title = value; }
+        public string Keywords { get => _keywords; set => _keywords = value; }
+        public string PubDate { get => _pubDate; set => _pubDate = value; }
+        public long ISBN { get => _ISBN; set => _ISBN = value; }
+
+        public object Clone()
+        {
+            return (new Book(this._author, this._title, this._keywords, this._pubDate, this._ISBN));
+        }
+
+        public override string ToString()
+        {
+            return (Title + ", " + Author + ", " + PubDate + ", Keywords: " + Keywords + ", ISBN: " + ISBN);
+        }
+    }
+}
---
+++ Module010/Topic02/Book/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+/***************************************************************
+* Name         : Book - interface
+* Author       : Anthony Hamlin
+* Created      : 10/24/2022
+* Course       : CIS 169 - C#
+* Version      : 1.0
+* OS           : Windows 10
+* IDE          : Visual Studio 2019 Community
+* Copyright    : This is my own original work based on
+*                      specifications issued by our instructor
+* Description  : This program creates a book class
+*                      Input :  Fields in the class - author, title, keywords, publication date and ISBN.
+*                      Output: a book instance
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or
+* unmodified. I have not given other fellow student(s) access
+* to my program.        
+***************************************************************/
+namespace Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // driver for Book class using interface
+            // Create a book object, book1,
+            Book book1 = new Book("Temple Grandin", "Visual Thinking", "Thinking in Pictures", "October 11, 2022", 0593418360);
+
+            // Clone book1 into a second object book2,
+            Book book2 = new Book();
+            book2 = (Book)book1.Clone();
+
+            // Display both the objects.
+            Console.WriteLine($"Book1: {book1.ToString()}");
+            Console.WriteLine($"Book2: {book2.ToString()}");
+        }
+    }
+}
