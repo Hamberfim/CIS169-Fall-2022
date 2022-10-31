c5e781429af53ae85b2d08fe003dd3a5bb9690b4
Hamberfim <adhamlingithub@gmail.com>
2022-10-31 11:21:05 -0500

    module 11 completed

---
+++ Module011/Topic02/BankAccount/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankAccount.Models;
+
+namespace BankAccount.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.EndingBalance = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(BankAccount.Models.BankAccount model)
+        {
+            model.Withdraw();
+            ViewBag.EndingBalance = model.Balance;
+            return View(model);
+        }
+    }
+}
---
+++ Module011/Topic02/BankAccount/Models/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankAccount.Models
+{
+    public class BankAccount
+    {
+        [Required(ErrorMessage = "Enter a numeric balance between 1 - 100000.")]
+        [Range(1, 100000)]
+        private decimal? _balance = 0;
+
+        [Required(ErrorMessage = "Enter a numeric amount between 1 - 10000.")]
+        [Range(1, 10000)]
+        private decimal? _amount = 0;
+
+        [Required(ErrorMessage = "Enter a numeric account number.")]
+        private int AccountNumber = 0;
+
+        public decimal Balance { get => _balance.Value; set => _balance = value; }
+        public decimal Amount { get => _amount.Value; set => _amount = value; }
+        public int AccountNumber1 { get => AccountNumber; set => AccountNumber = value; }
+
+        public void Withdraw()
+        {
+            _balance = (_balance - _amount);
+        }
+
+        public void Deposit()
+        {
+            _balance = (_balance + _amount);
+        }
+
+    }
+}
